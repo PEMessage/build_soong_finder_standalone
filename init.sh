979243d5225e30007b94e8722f2f8166b6df03b1
PEMessage <1165739182@qq.com>
2026-02-02 00:25:31 +0800

    add way to compile staticly

--- init.sh
+++ init.sh
@@ -8,3 +8,15 @@ if [ ! -f cmd/finder.go ] ; then
 fi
 
 go build -o finder ./cmd/
+
+if commnad -v zig > /dev/null ; then
+    # Thanks to: https://calabro.io/zig-cgo
+    # #            https://github.com/golang/go/issues/56386#issuecomment-1289185008
+    CC="zig cc -target x86_64-linux-musl" \
+        CGO_ENABLED=1 \
+        CGO_LDFLAGS="-static" \
+        GOOS=linux GOARCH=amd64 \
+        go build -a -ldflags '-extldflags "-static"' -ldflags=-linkmode=external -o finder_static ./cmd/
+fi
+
+
