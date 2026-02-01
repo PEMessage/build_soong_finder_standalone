b18fb6a46cd4be3d1aa2be2ff8250fe98a47f77e
PEMessage <1165739182@qq.com>
2026-02-02 00:20:30 +0800

    fix error path

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 set -xe
 
-if [ ! -f cmd/main.go ] ; then
-    git clone https://github.com/LineageOS/android_build_soong.git --depth 1
+if [ ! -f cmd/finder.go ] ; then
+    git clone https://github.com/LineageOS/android_build_soong.git --depth 1 --branch lineage-23.0
     cp android_build_soong/finder/{finder.go,fs,cmd} . -rf
     rm -rf android_build_soong
 fi
